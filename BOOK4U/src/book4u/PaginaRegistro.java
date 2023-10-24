71a8baef8a4a2bbba5f7aa90c95b3144a4cb8eb1
XavierC14 <74736448+XavierC14@users.noreply.github.com>
2023-10-24 16:52:00 +0200

    Add files via upload

---
+++ BOOK4U/src/book4u/PaginaRegistro.java
@@ -0,0 +1,14 @@
+package book4u;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author xavic
+ */
+public class PaginaRegistro {
+    
+}
