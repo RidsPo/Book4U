aa7699587ca3ce5890a6db3902e6019e6408f929
Achanta0 <123196913+Achanta0@users.noreply.github.com>
2023-10-30 19:18:10 +0100

    Add files via upload

--- BOOK4U/src/book4u/PaginaIniciarSesionPanel.java
+++ BOOK4U/src/book4u/PaginaIniciarSesionPanel.java
@@ -30,7 +30,7 @@ public class PaginaIniciarSesionPanel extends javax.swing.JPanel {
     private void initComponents() {
 
         jNombreUsuario = new javax.swing.JTextField();
-        jContraseñaUsuario = new javax.swing.JTextField();
+        jPasswordField1 = new javax.swing.JPasswordField();
         jBotonRegistro = new javax.swing.JButton();
         jBotonInicioSesion = new javax.swing.JButton();
         jFondo = new javax.swing.JLabel();
@@ -47,15 +47,9 @@ public class PaginaIniciarSesionPanel extends javax.swing.JPanel {
         });
         add(jNombreUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 530, 450, 50));
 
-        jContraseñaUsuario.setBackground(new java.awt.Color(184, 221, 140));
-        jContraseñaUsuario.setBorder(null);
-        jContraseñaUsuario.setMinimumSize(new java.awt.Dimension(64, 40));
-        jContraseñaUsuario.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jContraseñaUsuarioActionPerformed(evt);
-            }
-        });
-        add(jContraseñaUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 740, 450, 50));
+        jPasswordField1.setBackground(new java.awt.Color(184, 221, 140));
+        jPasswordField1.setBorder(null);
+        add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 742, 430, 60));
 
         jBotonRegistro.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Boton_Registro_PaginaIniciarSesion.png"))); // NOI18N
         jBotonRegistro.setBorder(null);
@@ -85,10 +79,6 @@ public class PaginaIniciarSesionPanel extends javax.swing.JPanel {
         add(jFondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1920, 1080));
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jContraseñaUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jContraseñaUsuarioActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jContraseñaUsuarioActionPerformed
-
     private void jBotonRegistroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBotonRegistroActionPerformed
         // TODO add your handling code here:
         JFrame marco = (JFrame) SwingUtilities.getWindowAncestor(this);
@@ -113,8 +103,8 @@ public class PaginaIniciarSesionPanel extends javax.swing.JPanel {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jBotonInicioSesion;
     private javax.swing.JButton jBotonRegistro;
-    private javax.swing.JTextField jContraseñaUsuario;
     private javax.swing.JLabel jFondo;
     private javax.swing.JTextField jNombreUsuario;
+    private javax.swing.JPasswordField jPasswordField1;
     // End of variables declaration//GEN-END:variables
 }
