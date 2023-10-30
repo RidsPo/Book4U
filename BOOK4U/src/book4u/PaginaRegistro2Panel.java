0849feb3e99cce5b3c90fef974c5936c3b9c4b27
danicolell <128613930+danicolell@users.noreply.github.com>
2023-10-30 20:07:54 +0100

    Add files via upload

--- BOOK4U/src/book4u/PaginaRegistro2Panel.java
+++ BOOK4U/src/book4u/PaginaRegistro2Panel.java
@@ -13,7 +13,8 @@ import javax.swing.SwingUtilities;
  * @author xavic
  */
 public class PaginaRegistro2Panel extends javax.swing.JPanel {
-
+private boolean passwordVisible1 = false;
+private boolean passwordVisible2 = false;
     /**
      * Creates new form PaginaRegistro2Panel
      */
@@ -37,6 +38,8 @@ public class PaginaRegistro2Panel extends javax.swing.JPanel {
         jPasswordField2 = new javax.swing.JPasswordField();
         jPasswordField1 = new javax.swing.JPasswordField();
         jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
         jFondo = new javax.swing.JLabel();
 
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -75,11 +78,21 @@ public class PaginaRegistro2Panel extends javax.swing.JPanel {
 
         jPasswordField2.setBackground(new java.awt.Color(184, 221, 140));
         jPasswordField2.setBorder(null);
-        add(jPasswordField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 692, 430, 30));
+        jPasswordField2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jPasswordField2ActionPerformed(evt);
+            }
+        });
+        add(jPasswordField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 692, 360, 30));
 
         jPasswordField1.setBackground(new java.awt.Color(184, 221, 140));
         jPasswordField1.setBorder(null);
-        add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 570, 430, 30));
+        jPasswordField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jPasswordField1ActionPerformed(evt);
+            }
+        });
+        add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 570, 360, 30));
 
         jButton1.setBackground(new java.awt.Color(204, 204, 204));
         jButton1.setText("Seleccionar imagen...");
@@ -90,16 +103,37 @@ public class PaginaRegistro2Panel extends javax.swing.JPanel {
         });
         add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 810, 460, 50));
 
+        jButton2.setText("VER");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(1110, 700, 70, -1));
+
+        jButton3.setText("VER");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(1110, 570, 70, -1));
+
         jFondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Fondo_PaginaRegistro2.png"))); // NOI18N
         add(jFondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
     }// </editor-fold>//GEN-END:initComponents
 
     private void jBotonRegistrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBotonRegistrarActionPerformed
         // TODO add your handling code here:
+        
         JFrame marco = (JFrame) SwingUtilities.getWindowAncestor(this);
+    
         marco.remove(this);
         marco.add(new PaginaPrincipalPanel());
         marco.setVisible(true);
+       
+    
+        
     }//GEN-LAST:event_jBotonRegistrarActionPerformed
 
     private void jBotonVolverActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBotonVolverActionPerformed
@@ -119,11 +153,43 @@ public class PaginaRegistro2Panel extends javax.swing.JPanel {
         }
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    private void jPasswordField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jPasswordField2ActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_jPasswordField2ActionPerformed
+
+    private void jPasswordField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jPasswordField1ActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_jPasswordField1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+         passwordVisible2 = !passwordVisible2;
+    if (passwordVisible2) {
+        jPasswordField2.setEchoChar((char) 0); // Mostrar la contraseña
+    } else {
+        jPasswordField2.setEchoChar('*'); // Ocultar la contraseña con asteriscos
+    }
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+     passwordVisible1 = !passwordVisible1;
+    if (passwordVisible1) {
+        jPasswordField1.setEchoChar((char) 0); // Mostrar la contraseña
+    } else {
+        jPasswordField1.setEchoChar('*'); // Ocultar la contraseña con asteriscos
+    }
+    }//GEN-LAST:event_jButton3ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jBotonRegistrar;
     private javax.swing.JButton jBotonVolver;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jFondo;
     private javax.swing.JPasswordField jPasswordField1;
     private javax.swing.JPasswordField jPasswordField2;
