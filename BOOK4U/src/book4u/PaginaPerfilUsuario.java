b1e449137a4cc9a3b5c020b40efa6336dd9f7845
danicolell <128613930+danicolell@users.noreply.github.com>
2023-10-31 15:49:38 +0100

    Add files via upload

--- BOOK4U/src/book4u/PaginaPerfilUsuario.java
+++ BOOK4U/src/book4u/PaginaPerfilUsuario.java
@@ -4,6 +4,9 @@
  */
 package book4u;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 /**
  *
  * @author Daniel
@@ -26,31 +29,100 @@ public class PaginaPerfilUsuario extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jTextField2 = new javax.swing.JTextField();
+        jTextField3 = new javax.swing.JTextField();
+        jTextField4 = new javax.swing.JTextField();
+        jTextField5 = new javax.swing.JTextField();
+        jTextField6 = new javax.swing.JTextField();
+        jTextField7 = new javax.swing.JTextField();
+        jPasswordField1 = new javax.swing.JPasswordField();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jTextField2.setBackground(new java.awt.Color(184, 221, 140));
+        jTextField2.setBorder(null);
+        add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(1050, 680, 210, 30));
+
+        jTextField3.setBackground(new java.awt.Color(184, 221, 140));
+        jTextField3.setBorder(null);
+        add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(1050, 540, 220, 30));
+
+        jTextField4.setBackground(new java.awt.Color(184, 221, 140));
+        jTextField4.setBorder(null);
+        add(jTextField4, new org.netbeans.lib.awtextra.AbsoluteConstraints(1050, 400, 220, 30));
+
+        jTextField5.setBackground(new java.awt.Color(184, 221, 140));
+        jTextField5.setBorder(null);
+        add(jTextField5, new org.netbeans.lib.awtextra.AbsoluteConstraints(1400, 400, 220, 30));
+
+        jTextField6.setBackground(new java.awt.Color(184, 221, 140));
+        jTextField6.setBorder(null);
+        add(jTextField6, new org.netbeans.lib.awtextra.AbsoluteConstraints(1400, 540, 210, 30));
+
+        jTextField7.setBackground(new java.awt.Color(184, 221, 140));
+        jTextField7.setBorder(null);
+        add(jTextField7, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 710, 280, 30));
+
+        jPasswordField1.setBackground(new java.awt.Color(184, 221, 140));
+        jPasswordField1.setBorder(null);
+        add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(1400, 690, 170, 20));
+
+        jButton1.setText("VER");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(1570, 690, 60, -1));
+
+        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/BotonIniciarSesion.png"))); // NOI18N
+        jButton2.setBorder(null);
+        jButton2.setContentAreaFilled(false);
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(1360, 800, 340, 100));
+
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/paginaPerfilUsuario.png"))); // NOI18N
-        jLabel1.setText("jLabel1");
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jLabel1)
-                .addGap(0, 778, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jLabel1)
-                .addGap(0, 213, Short.MAX_VALUE))
-        );
-
+        jLabel1.setFocusable(false);
+        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
         jLabel1.getAccessibleContext().setAccessibleName("");
     }// </editor-fold>//GEN-END:initComponents
+private boolean passwordVisible1 = false;
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+           passwordVisible1 = !passwordVisible1;
+    if (passwordVisible1) {
+        jPasswordField1.setEchoChar((char) 0); // Mostrar la contraseña
+    } else {
+        jPasswordField1.setEchoChar('*'); // Ocultar la contraseña con asteriscos
+    }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+            JFrame marco = (JFrame) SwingUtilities.getWindowAncestor(this);
+        marco.remove(this);
+        marco.add(new PaginaPrincipalPanel ());
+        marco.setVisible(true);
+    }//GEN-LAST:event_jButton2ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JPasswordField jPasswordField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
+    private javax.swing.JTextField jTextField5;
+    private javax.swing.JTextField jTextField6;
+    private javax.swing.JTextField jTextField7;
     // End of variables declaration//GEN-END:variables
 }
